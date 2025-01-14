d820cdc5302e9875a141d2e32e71a26ca30f02a4
Eldar Iusupzhanov <ferrarijed@gmail.com>
2025-01-14 18:40:36 +0800

    fix & test

---
+++ apps/demos/testing/widgets/datagrid/PopupEditing.test.js
@@ -0,0 +1,25 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector as $ } from 'testcafe';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
+
+fixture('DataGrid.PopupEditing')
+  .page('http://localhost:8080/')
+  .before(async (ctx) => {
+    ctx.initialWindowSize = [900, 600];
+  });
+
+runManualTest('DataGrid', 'PopupEditing', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test.only('PopupEditing', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    
+    await testScreenshot(t, takeScreenshot, 'datagrid_popup_editing_1_desktop.png');
+
+    await t.click($('.dx-link-edit').nth(0));
+    await testScreenshot(t, takeScreenshot, 'datagrid_popup_editing_2_desktop.png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
